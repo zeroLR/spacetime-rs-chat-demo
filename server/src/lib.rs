673c414c8bde4b94df690de739a29d0857682600
zeroLR <ceyanglab@gmail.com>
2025-03-13 22:55:26 +0800

    Initialize Spacetime module with user and message handling

---
+++ server/src/lib.rs
@@ -0,0 +1,107 @@
+use spacetimedb::{sats::de::ValidNames, Identity, ReducerContext, Table, Timestamp};
+
+#[spacetimedb::table(name = user, public)]
+pub struct User {
+    #[primary_key]
+    identity: Identity,
+    name: Option<String>,
+    online: bool,
+}
+
+#[spacetimedb::table(name = message, public)]
+pub struct Message {
+    sender: Identity,
+    sent: Timestamp,
+    text: String,
+}
+
+#[spacetimedb::reducer(init)]
+pub fn init(_ctx: &ReducerContext) {
+    // Called when the module is initially published
+}
+
+#[spacetimedb::reducer(client_connected)]
+// Called when a client connects to the SpacetimeDB
+pub fn client_connected(ctx: &ReducerContext) {
+    log::info!("Client connected with identity {:?}", ctx.sender);
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        // If this is a returning user, i.e. we already have a `User` with this `Identity`,
+        // set `online: true`, but leave `name` and `identity` unchanged.
+        ctx.db.user().identity().update(User {
+            online: true,
+            ..user
+        });
+    } else {
+        // If this is a new user, create a `User` row for the `Identity`,
+        // which is online, but hasn't set a name.
+        ctx.db.user().insert(User {
+            name: None,
+            identity: ctx.sender,
+            online: true,
+        });
+    }
+}
+
+#[spacetimedb::reducer(client_disconnected)]
+// Called when a client disconnects from SpacetimeDB
+pub fn identity_disconnected(ctx: &ReducerContext) {
+    log::info!("Client disconnected with identity {:?}", ctx.sender);
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        ctx.db.user().identity().update(User {
+            online: false,
+            ..user
+        });
+    } else {
+        // This branch should be unreachable,
+        // as it doesn't make sense for a client to disconnect without connecting first.
+        log::warn!(
+            "Disconnect event for unknown user with identity {:?}",
+            ctx.sender
+        );
+    }
+}
+
+#[spacetimedb::reducer]
+pub fn set_name(ctx: &ReducerContext, name: String) -> Result<(), String> {
+    let name = validate_name(name)?;
+    if let Some(user) = ctx.db.user().identity().find(ctx.sender) {
+        ctx.db.user().identity().update(User {
+            name: Some(name),
+            ..user
+        });
+        Ok(())
+    } else {
+        Err("Cannot set name for unknown user".to_string())
+    }
+}
+
+#[spacetimedb::reducer]
+/// Clients invoke this reducer to send messages.
+pub fn send_message(ctx: &ReducerContext, text: String) -> Result<(), String> {
+    let text = validate_message(text)?;
+    log::info!("{}", text);
+    ctx.db.message().insert(Message {
+        sender: ctx.sender,
+        text,
+        sent: ctx.timestamp,
+    });
+    Ok(())
+}
+
+/// Takes a name and checks if it's acceptable as a user's name.
+fn validate_name(name: String) -> Result<String, String> {
+    if name.is_empty() {
+        Err("Names must not be empty".to_string())
+    } else {
+        Ok(name)
+    }
+}
+
+/// Takes a message's text and checks if it's acceptable to send.
+fn validate_message(text: String) -> Result<String, String> {
+    if text.is_empty() {
+        Err("Messages must not be empty".to_string())
+    } else {
+        Ok(text)
+    }
+}
