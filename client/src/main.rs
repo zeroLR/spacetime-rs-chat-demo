35ccc12a52ecc92da434ce397aca9b4750adf216
zeroLR <ceyanglab@gmail.com>
2025-03-13 22:57:45 +0800

    Add initial client module with configuration, types, and table bindings

---
+++ client/src/main.rs
@@ -0,0 +1,222 @@
+mod module_bindings;
+use dotenv::dotenv;
+use module_bindings::*;
+use spacetimedb_sdk::{
+    DbContext, Error, Event, Identity, Status, Table, TableWithPrimaryKey, credentials,
+};
+use std::env;
+use tokio::signal;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let host = env::var("HOST").expect("HOST must be set");
+    let db_name = env::var("DB_NAME").expect("DB_NAME must be set");
+
+    // Connect to the database
+    let ctx = connect_to_db(&host, &db_name);
+
+    // Register callbacks to run in response to database events.
+    register_callbacks(&ctx);
+
+    // Subscribe to SQL queries in order to construct a local partial replica of the database.
+    subscribe_to_tables(&ctx);
+
+    // Spawn a thread, where the connection will process messages and invoke callbacks.
+    ctx.run_threaded();
+
+    // Handle CLI input
+    user_input_loop(&ctx);
+
+    // Wait for a shutdown signal
+    signal::ctrl_c()
+        .await
+        .expect("Failed to listen for shutdown signal");
+    println!("Shutdown signal received, exiting...");
+}
+
+/// Load credentials from a file and connect to the database.
+fn connect_to_db(host: &str, db_name: &str) -> DbConnection {
+    let db_name = db_name.to_string();
+    let db_name_clone = db_name.clone();
+    DbConnection::builder()
+        // Register our `on_connect` callback, which will save our auth token.
+        .on_connect(move |ctx, identity, token| on_connected(ctx, identity, token, &db_name))
+        // Register our `on_connect_error` callback, which will print a message, then exit the process.
+        .on_connect_error(on_connect_error)
+        // Our `on_disconnect` callback, which will print a message, then exit the process.
+        .on_disconnect(on_disconnected)
+        // If the user has previously connected, we'll have saved a token in the `on_connect` callback.
+        // In that case, we'll load it and pass it to `with_token`,
+        // so we can re-authenticate as the same `Identity`.
+        .with_token(
+            creds_store(&db_name_clone)
+                .load()
+                .expect("Error loading credentials"),
+        )
+        // Set the database name we chose when we called `spacetime publish`.
+        .with_module_name(&db_name_clone)
+        // Set the URI of the SpacetimeDB host that's running our database.
+        .with_uri(host)
+        // Finalize configuration and connect!
+        .build()
+        .expect("Failed to connect.")
+}
+
+fn creds_store(db_name: &str) -> credentials::File {
+    credentials::File::new(db_name)
+}
+
+/// Our `on_connect` callback: save our credentials to a file.
+fn on_connected(_ctx: &DbConnection, _identity: Identity, token: &str, db_name: &str) {
+    let masked_token = if token.len() > 8 {
+        format!("...{}", &token[token.len() - 8..])
+    } else {
+        token.to_string()
+    };
+    print!("Connected with token: {}\n", masked_token);
+    print!("Connected as identity: {}\n", _identity);
+    if let Err(e) = creds_store(db_name).save(token) {
+        eprintln!("Failed to save credentials: {:?}", e);
+    }
+}
+
+/// Our `on_connect_error` callback: print the error, then exit the process.
+fn on_connect_error(_ctx: &ErrorContext, err: Error) {
+    eprintln!("Connection error: {:?}", err);
+    std::process::exit(1);
+}
+
+/// Our `on_disconnect` callback: print a note, then exit the process.
+fn on_disconnected(_ctx: &ErrorContext, err: Option<Error>) {
+    if let Some(err) = err {
+        eprintln!("Disconnected: {}", err);
+        std::process::exit(1);
+    } else {
+        println!("Disconnected.");
+        std::process::exit(0);
+    }
+}
+
+/// Register all the callbacks our app will use to respond to database events.
+fn register_callbacks(ctx: &DbConnection) {
+    // When a new user joins, print a notification.
+    ctx.db.user().on_insert(on_user_inserted);
+
+    // When a user's status changes, print a notification.
+    ctx.db.user().on_update(on_user_updated);
+
+    // When a new message is received, print it.
+    ctx.db.message().on_insert(on_message_inserted);
+
+    // When we fail to set our name, print a warning.
+    ctx.reducers.on_set_name(on_name_set);
+
+    // When we fail to send a message, print a warning.
+    ctx.reducers.on_send_message(on_message_sent);
+}
+
+/// Our `User::on_insert` callback:
+/// if the user is online, print a notification.
+fn on_user_inserted(_ctx: &EventContext, user: &User) {
+    if user.online {
+        println!("User {} connected.", user_name_or_identity(user));
+    }
+}
+
+fn user_name_or_identity(user: &User) -> String {
+    user.name
+        .clone()
+        .unwrap_or_else(|| user.identity.to_hex().to_string())
+}
+
+/// Our `User::on_update` callback:
+/// print a notification about name and status changes.
+fn on_user_updated(_ctx: &EventContext, old: &User, new: &User) {
+    if old.name != new.name {
+        println!(
+            "User {} renamed to {}.",
+            user_name_or_identity(old),
+            user_name_or_identity(new)
+        );
+    }
+    if old.online && !new.online {
+        println!("User {} disconnected.", user_name_or_identity(new));
+    }
+    if !old.online && new.online {
+        println!("User {} connected.", user_name_or_identity(new));
+    }
+}
+
+/// Our `Message::on_insert` callback: print new messages.
+fn on_message_inserted(ctx: &EventContext, message: &Message) {
+    if let Event::Reducer(_) = ctx.event {
+        print_message(ctx, message)
+    }
+}
+
+fn print_message(ctx: &impl RemoteDbContext, message: &Message) {
+    let sender = ctx
+        .db()
+        .user()
+        .identity()
+        .find(&message.sender.clone())
+        .map(|u| user_name_or_identity(&u))
+        .unwrap_or_else(|| "unknown".to_string());
+    println!("{}: {}", sender, message.text);
+}
+
+/// Our `on_set_name` callback: print a warning if the reducer failed.
+fn on_name_set(ctx: &ReducerEventContext, name: &String) {
+    if let Status::Failed(err) = &ctx.event.status {
+        eprintln!("Failed to change name to {:?}: {}", name, err);
+    }
+}
+
+/// Our `on_send_message` callback: print a warning if the reducer failed.
+fn on_message_sent(ctx: &ReducerEventContext, text: &String) {
+    if let Status::Failed(err) = &ctx.event.status {
+        eprintln!("Failed to send message {:?}: {}", text, err);
+    }
+}
+
+/// Register subscriptions for all rows of both tables.
+fn subscribe_to_tables(ctx: &DbConnection) {
+    ctx.subscription_builder()
+        .on_applied(on_sub_applied)
+        .on_error(on_sub_error)
+        .subscribe(["SELECT * FROM user", "SELECT * FROM message"]);
+}
+
+/// Our `on_subscription_applied` callback:
+/// sort all past messages and print them in timestamp order.
+fn on_sub_applied(ctx: &SubscriptionEventContext) {
+    let mut messages = ctx.db.message().iter().collect::<Vec<_>>();
+    messages.sort_by_key(|m| m.sent);
+    for message in messages {
+        print_message(ctx, &message);
+    }
+    println!("Fully connected and all subscriptions applied.");
+    println!("Use /name to set your name, or type a message!");
+}
+
+/// Or `on_error` callback:
+/// print the error, then exit the process.
+fn on_sub_error(_ctx: &ErrorContext, err: Error) {
+    eprintln!("Subscription failed: {}", err);
+    std::process::exit(1);
+}
+
+/// Read each line of standard input, and either set our name or send a message as appropriate.
+fn user_input_loop(ctx: &DbConnection) {
+    for line in std::io::stdin().lines() {
+        let Ok(line) = line else {
+            panic!("Failed to read from stdin.");
+        };
+        if let Some(name) = line.strip_prefix("/name ") {
+            ctx.reducers.set_name(name.to_string()).unwrap();
+        } else {
+            ctx.reducers.send_message(line).unwrap();
+        }
+    }
+}
